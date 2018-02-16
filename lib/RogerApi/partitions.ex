df2cff02ac3862ca7bb098511f93445dca8cedbb
ingsissam <33227372+ingsissam@users.noreply.github.com>
2018-02-16 11:51:14 -0400

    Feat: Rework of the module

--- lib/RogerApi/partitions.ex
+++ lib/RogerApi/partitions.ex
@@ -6,32 +6,39 @@ defmodule RogerApi.Partitions do
   def run(parts) do
     parts
     |>Keyword.values()
-    |>create_the_structure
-    |> dispose_unused_keys
-
-
-  end
-
-  defp create_the_structure(keys) do
-    keys
-    |>Enum.flat_map(&(Enum.flat_map(&1,fn {state_name,ques} ->  ques
-                                                                |> Enum.flat_map(fn {partition_name,partition} -> partition
-                                                                                                                  |> Enum.map(fn {que_name,values} -> values
-                                                                                                                                                      |>Map.merge(%{
-      "paused"              => "#{state_name}",
-      "partition_name"       => "#{partition_name}",
-      "queue_name"           =>    que_name,
-      "qualified_queue_name" =>"#{partition_name}-#{que_name}"})
-    end)  end) end)))
-  end
-
-  defp dispose_unused_keys(partition_value) do
-    partition_value
-    |> Enum.map(&(Map.drop(&1,[:consumer_count,:max_workers,:paused])))
-    |> Enum.map(fn k -> Map.put(k,"count", Map.get(k, :message_count))  end )
-    |> Enum.map(&(Map.drop(&1,[:message_count])))
+    |>Enum.flat_map(&Map.values/1)
+    |>Enum.flat_map(&of_partitions_to_queus/1)
+    |>Enum.map(&dispose_unused_keys/1)
 
   end
 
+  defp of_partitions_to_queus(partition) do
+    partition
+    |>Enum.flat_map(&final_queus_format/1)
+
+  end
+
+  defp final_queus_format {partition_name,queus} do
+    Enum.map(queus, fn {queu_name,queu_value} ->
+      queu_value
+      |> Map.merge(%{
+         "partition_name"       =>  partition_name,
+         "queue_name"           =>   queu_name,
+         "qualified_queue_name" =>  "#{partition_name}-#{queu_name}",
+          "count"               =>  queu_value.message_count,
+          "paused"              =>  paused?(queu_value.paused)})  end)
+
+
+    
+  end
+
+  defp dispose_unused_keys(q) do
+     q
+    |> Map.drop([:consumer_count,:max_workers,:paused,:message_count])
+    
+  end
+
+  defp paused?(true), do: "paused"
+  defp paused?(false), do: "running"
 
 end
