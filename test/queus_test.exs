850ea2ddaf7ab3c85f26aa28cfc2741d9ec0b3b4
ingsissam <33227372+ingsissam@users.noreply.github.com>
2018-02-16 15:02:55 -0400

    doc: most explicit test example

--- test/queus_test.exs
+++ test/queus_test.exs
@@ -6,7 +6,7 @@ defmodule RogerapiTest.Jobs do
   alias RogerApi.Queus
   alias RogerApi.Support
 
-  test "given an input of  a list of nodes, partitions and queus the output should be a list queus" do
+  test "given an input of  a list of nodes, partitions and queus the output should be a list of queus" do
    quein =Support.inque()
    queout =Support.outque()
    assert MapSet.new(queout) == MapSet.new(Queus.nodes_to_queus(quein))
